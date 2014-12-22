700747d21e24626f1821251cc4f2dd3047a0b77e
Justin Chmura <justin.chmura@gmail.com>
2014-12-22 09:25:17 -0700

    Improved Beautiful Strings submission

--- easy/beautiful-strings/main.js
+++ easy/beautiful-strings/main.js
@@ -1,33 +1,21 @@
-(function () {
-
-  function computeOccurrences(line) {
-    var chars = line.toLowerCase().split('').sort(), occurrences = [];
-    for (var i = 0; i < chars.length; i++) {
-      var c = chars[i]
-        , occ = (chars.lastIndexOf(c) - chars.indexOf(c)) + 1;
-      occurrences.push(occ);
-      i += occ - 1;
-    }
-    return occurrences;
-  }
-  
-  function computeScore(occurrences) {
-    var weight = 26, sum = 0;
-    occurrences.sort();
-    for (var i = occurrences.length - 1; i >= 0; i--) {
-      var multi = occurrences[i];
-      occurrences[i] = multi * weight--;
-      sum += occurrences[i];
-    }
-    return sum;
-  }
-
-  var fs = require('fs');
-  fs.readFile(process.argv[2], function (err, data) {
-    var lines = data.toString().replace(/[^\w\n]|_|\r/g, '').split('\n');
-    for (var i = 0; i < lines.length; i++) {
-      console.log(computeScore(computeOccurrences(lines[i])));
-    }
-    process.exit(0);
+require('fs').readFile(process.argv[2], 'utf8', function (err, data) {
+  data.replace(/\r/g, '').split('\n').map(function (line) {
+    return line.toLowerCase().replace(/[^a-z]/ig, '').split('').sort();
+  }).forEach(function (chars) {
+    var weight = 26;
+    var counts = chars.reduce(function (prev, c, i, self) {
+      if (!prev.hasOwnProperty(c)) { prev[c] = 1; }
+      else { prev[c] += 1; }
+      return prev;
+    }, {}); 
+    var sum = Object.keys(counts).sort(function (left, right) {
+      if (counts[left] > counts[right]) { return -1; }
+      if (counts[left] < counts[right]) { return 1; }
+      return 0;
+    }).reduce(function (prev, key) {
+      prev += counts[key] * weight--;
+      return prev;
+    }, 0);
+    console.log(sum);
   });
-} ());
+});
